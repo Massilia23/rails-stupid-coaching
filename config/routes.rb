799a7e30b338068eeb2330b34065b0ec08d02af9
Massilia Bakhouche <massiliabakhouche@gmail.com>
2023-02-20 12:23:01 +0100

    coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  get "ask", to: "pages#ask"
+  get "answer", to: "pages#answer"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
